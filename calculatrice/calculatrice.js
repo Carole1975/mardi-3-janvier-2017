83c42b189d481987a1153657633799ba280533bd
Carole <carole.velarde@orange.fr>
2017-01-03 13:32:16 +0100

    1er commit

---
+++ calculatrice/calculatrice.js
@@ -0,0 +1,5 @@
+var multiplication = ("36*28=");
+console.log ("36 * 28 ="); // Affiche la multiplication.
+
+var résultat = (36 * 28);
+console.log (36 * 28); // Affiche le résultat.
---
+++ mardi3janvier/index.js
@@ -0,0 +1,11 @@
+var nombre = prompt [2, 3];
+
+var chiffre = prompt [2, "2"];
+
+if (typeof nombre == typeof chiffre) {
+alert ("ce ne sont pas les memes");
+}
+
+else {
+alert ("ce sont les memes")
+}
